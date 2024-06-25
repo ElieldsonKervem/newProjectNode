3c34375458a2e82e5fb454e88a850bee45dfe755
ElieldsonKervem <kervem.elieldson@academico.ifrn.edu.br>
2024-06-25 12:26:00 -0300

    more modules add

--- src/routers/cateogories.routers.ts
+++ src/routers/cateogories.routers.ts
@@ -1,29 +1,38 @@
 import { Router } from 'express';
 
 import { Category } from '../model/Category';
+import { CategorieRepository } from '../repositories/categoriesRepository';
+
+
+const categoriesRepository = new CategorieRepository();
+CategorieRepository
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+
 
 categoriesRoutes.post("/",(request, response) => {
     const { name, description } = request.body;
+
+    const categoryAlreadyExist = categoriesRepository.findByname(name)
+
+    if(categoryAlreadyExist){
+        return response.status(400).json({error: "Catefory already exist!"})
+    }
 
     if (!name || !description) {
         return response.status(400).json({ error: 'Nome e descrição são necessarios' });
     }
 
-    const category = new Category();
+     categoriesRepository.create({name,description})
 
-     Object.assign(category,{
-        name,
-        description,
-        created_at:new Date()
-     })
-
-    categories.push(category)   
-
-    return response.status(201).json({category});
+    return response.status(201).send();
 });
+
+categoriesRoutes.get("/",(request,response)=>{
+     const all = categoriesRepository.list();
+
+     return response.json(all)
+})
 
 export { categoriesRoutes };
---
+++ src/services/createCategoryService.ts
@@ -0,0 +1,8 @@
+
+
+class CreateCategoryService {
+
+}
+
+
+export {CreateCategoryService}
